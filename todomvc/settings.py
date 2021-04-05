c06dbff9d50f5328ba218b9ca63c4a4f48464133
Niels Lerche Sørensen <nielslerches@gmail.com>
2021-04-05 16:43:47 +0200

    debug default to false

--- todomvc/settings.py
+++ todomvc/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "0") == "0"
+DEBUG = os.getenv("DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = ["todomvc-django.herokuapp.com"]
 
